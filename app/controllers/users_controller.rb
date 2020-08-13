6d4c33611411863c6f3870b301568f26cd09443f
michael thomas <michael.thomas2014@gmail.com>
2020-08-13 13:02:15 +0000

    Automatically backed up by Learn

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  
+  def new
+     @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+  
+  
+end
